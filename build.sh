b6fdf317ce027cbc00685dd300faa434776bb91a
Ricardo Camacho <risqadev@gmail.com>
2025-12-04 18:49:16 -0300

    feat/melhora build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-rm ./crm.bin ./modules/*;
-cobc -I ./src/copybooks/ -c ./src/CRMP-ALT.cbl -o ./modules/CRMP-ALT.o;
-cobc -I ./src/copybooks/ -c ./src/CRMP-CAD.cbl -o ./modules/CRMP-CAD.o;
-cobc -I ./src/copybooks/ -c ./src/CRMP-CON.cbl -o ./modules/CRMP-CON.o;
-cobc -I ./src/copybooks/ -c ./src/CRMP-EXC.cbl -o ./modules/CRMP-EXC.o;
-cobc -I ./src/copybooks/ -c ./src/CRMP-LAT.cbl -o ./modules/CRMP-LAT.o;
-cobc -I ./src/copybooks/ -c ./src/CRMP-LIN.cbl -o ./modules/CRMP-LIN.o;
-cobc -I ./src/copybooks/ -c ./src/CRMP-MAN.cbl -o ./modules/CRMP-MAN.o;
-cobc -I ./src/copybooks/ -c ./src/CRMP-SIT.cbl -o ./modules/CRMP-SIT.o;
-cobc -I ./src/copybooks/ -x ./src/CRMP-000.cbl ./modules/*.o -o ./crm.bin;
+# Cria o diretório modules se não existir
+[ -d ./modules ] || mkdir ./modules
+
+# Remove arquivos existentes
+rm -f ./crm.bin ./modules/*
+
+# Compila os módulos
+cobc -I ./src/copybooks/ -c ./src/CRMP-ALT.cbl -o ./modules/CRMP-ALT.o
+cobc -I ./src/copybooks/ -c ./src/CRMP-CAD.cbl -o ./modules/CRMP-CAD.o
+cobc -I ./src/copybooks/ -c ./src/CRMP-CON.cbl -o ./modules/CRMP-CON.o
+cobc -I ./src/copybooks/ -c ./src/CRMP-EXC.cbl -o ./modules/CRMP-EXC.o
+cobc -I ./src/copybooks/ -c ./src/CRMP-LAT.cbl -o ./modules/CRMP-LAT.o
+cobc -I ./src/copybooks/ -c ./src/CRMP-LIN.cbl -o ./modules/CRMP-LIN.o
+cobc -I ./src/copybooks/ -c ./src/CRMP-MAN.cbl -o ./modules/CRMP-MAN.o
+cobc -I ./src/copybooks/ -c ./src/CRMP-SIT.cbl -o ./modules/CRMP-SIT.o
+
+# Compila o executável
+cobc -I ./src/copybooks/ -x ./src/CRMP-000.cbl ./modules/*.o -o ./crm.bin
